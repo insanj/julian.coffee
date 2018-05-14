17735b4110240c1465e3b74c60528f048ad80e29
Julian Weiss <insanj@gmail.com>
2018-05-13 21:06:24 -0400

    add loading indicator from codepen, format

--- julian.coffee/static/coffee.js
+++ julian.coffee/static/coffee.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 	var coffeeURL = window.location.href + "coffeeTime";
 	var coffeeScraper = new CoffeeScraper(null, coffeeURL);
 	coffeeScraper.roast(function (coffeeObjects) {
+		$("#loading").remove();
 		var coffeeParentDivId = "patrons";
 		var coffeeRenderer = new CoffeeRenderer(coffeeParentDivId);
 		coffeeRenderer.brew(coffeeObjects);
